1c63d7e0fbf053f248097d009f77a20c294fd730
SMhd <SMhd3000@gmail.com>
2023-01-28 23:51:49 +0330

    fix: :bug: fix goal_test for forward_search

--- ForwardSearch.py
+++ ForwardSearch.py
@@ -4,15 +4,16 @@ import collections
 
 import heapdict
 
+
 def forward_search(goal_state, initial_state, actions):
     # fringe=PriorityQueue()
-    fringe =heapdict.heapdict()
-    fringe[initial_state]=5000
+    fringe = heapdict.heapdict()
+    fringe[initial_state] = 5000
     in_fringe = [initial_state.hash()]
     explored = []
 
     while fringe:
-        
+
         tmp = fringe.popitem()
         current_state = tmp[0]
         in_fringe.pop(0)
@@ -25,30 +26,30 @@ def forward_search(goal_state, initial_state, actions):
                 return
             else:
                 if successor.hash() not in in_fringe and successor.hash() not in explored:
-                    fringe[successor]= ignorePreconditions(actions,successor,goal_state)
+                    fringe[successor] = ignorePreconditions(
+                        actions, successor, goal_state)
                     # fringe.append(successor)
                     in_fringe.append(successor.hash())
-                    
-
 
 
 def get_successors(state, actions):
     result = []
     for action in actions:
         if action.is_relevantForward(state):
-            successor=State(state,action,state.positive_literals,state.negative_literals)
+            successor = State(
+                state, action, state.positive_literals, state.negative_literals)
             action.progress(successor)
-            result.append(successor)      
+            result.append(successor)
     return result
 
 
 def goal_test(state, goal_state):
-    for positive_literal in state.positive_literals:
-        if positive_literal not in goal_state.positive_literals:
+    for positive_literal in goal_state.positive_literals:
+        if positive_literal not in state.positive_literals:
             return False
 
-    for negative_literal in state.negative_literals:
-        if negative_literal in goal_state.positive_literals:
+    for negative_literal in goal_state.negative_literals:
+        if negative_literal in state.positive_literals:
             return False
     return True
 
@@ -58,41 +59,38 @@ def print_solution(state):
         return
     print_solution(state.parent)
     print(state.action.to_string())
-    
-                
-                
-def ignorePreconditions(actions,state,goal):
-    st = State(state.parent,state.action,state.positive_literals,state.negative_literals)
-    cost =0 
+
+
+def ignorePreconditions(actions, state, goal):
+    st = State(state.parent, state.action,
+               state.positive_literals, state.negative_literals)
+    cost = 0
     for act in actions:
-        for poseffect in act.add_list:  
-            if(( poseffect not in st.positive_literals) & (poseffect in goal.positive_literals)):
+        for poseffect in act.add_list:
+            if ((poseffect not in st.positive_literals) & (poseffect in goal.positive_literals)):
                 st.positive_literals.append(poseffect)
-                cost+=1
+                cost += 1
                 for negeff in act.delete_list:
-                    if(negeff in st.positive_literals):
-                        st.positive_literals.remove(negeff)             
-    if (compareList(goal.positive_literals,st.positive_literals)):
+                    if (negeff in st.positive_literals):
+                        st.positive_literals.remove(negeff)
+    if (compareList(goal.positive_literals, st.positive_literals)):
         # print("here ", cost)
-        return cost 
-    
-def ignoreDeleteList(state,goal):
-    cost=0
+        return cost
+
+
+def ignoreDeleteList(state, goal):
+    cost = 0
     for p in goal.positive_literals:
         if p not in state.positive_literals:
-            cost+=1
+            cost += 1
     return cost
-        
-        
-#This method tests for the equality of the lists by comparing frequency of each element
-# in first list with the second list. This method also does not take 
-# into account the order of the elements of the list.        
-def compareList(l1,l2):
-   if(collections.Counter(l1)==collections.Counter(l2)):
-      return True
-   else:
-      return True
-            
-    
-                
-        
+
+
+# This method tests for the equality of the lists by comparing frequency of each element
+# in first list with the second list. This method also does not take
+# into account the order of the elements of the list.
+def compareList(l1, l2):
+    if (collections.Counter(l1) == collections.Counter(l2)):
+        return True
+    else:
+        return True
