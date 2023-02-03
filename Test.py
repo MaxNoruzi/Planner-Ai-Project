d18d0fca9152a0947f1336ceb069edf46ccca68f
KimiyaPoozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2023-02-03 19:48:11 +0330

    fix: :bug:

--- Test.py
+++ Test.py
@@ -9,10 +9,12 @@ def main():
     actions = get_actions()
 
     print("Planning...")
-    # initial_state = State(None, None, positive_literals=['atflataxle', 'atsparetrunk'], negative_literals=['atspareaxle', 'atspareground', 'atflatground'])
-    # goal_state = State(None, None, positive_literals=['atspareaxle', 'atflatground'], negative_literals=['atsparetrunk', 'atspareground', 'atflataxle'])
-    initial_state = State(None, None, positive_literals=['TruckAt1', 'BAt1','Bonfloor','AAt2','CAt3','Aplate4','Cplate5'], negative_literals=['AonB', 'BonC'])
-    goal_state = State(None, None, positive_literals=['AAt3','BAt3','AonB', 'BonC','Aplate5','Bplate5'], negative_literals=[])
+
+    initial_state = State(None, None, positive_literals=['TruckAt1', 'BoxBAt1','Bonfloor','BoxAAt2','BoxCAt3','Aplate4','Cplate5','clearA','clearC','clearB'], negative_literals=[])
+    goal_state = State(None, None, positive_literals=['BonC','Bplate5','BAt3','Cplate5'], negative_literals=[])
+    
+    # initial_state = State(None, None, positive_literals=['TruckAt1', 'BoxBAt1','Bplate5','clearB'], negative_literals=[])
+    # goal_state = State(None, None, positive_literals=['Bonfloor'], negative_literals=[])
     backward_search(goal_state, initial_state, actions)
     # forward_search(goal_state, initial_state, actions)
 if __name__ == "__main__":
--- depotsdomain.py
+++ depotsdomain.py
@@ -11,9 +11,13 @@ def get_actions():
             if(loc==loc1):
                 continue
             else:
-                drive= Action(name ='drive from' + loc + ' to '+loc1,positive_preconditions=['TruckAt'+loc],\
-                    negative_preconditions=[],add_list=["TruckAt"+loc1],delete_list=["TruckAt"+loc])
-                actions.append(drive)
+                emptyDrive=  Action(name ='drive from' + loc + ' to '+loc1,positive_preconditions=['TruckAt'+loc],\
+                        negative_preconditions=[],add_list=["TruckAt"+loc1],delete_list=["TruckAt"+loc])
+                actions.append(emptyDrive)
+                for b in boxes:
+                    drive= Action(name ='drive from' + loc + ' to '+loc1,positive_preconditions=['TruckAt'+loc,b+'onTruck'],\
+                        negative_preconditions=[],add_list=["TruckAt"+loc1,b+'At'+loc1],delete_list=["TruckAt"+loc])
+                    actions.append(drive)
 
         for b in boxes:
             for bx in boxes:
@@ -22,13 +26,13 @@ def get_actions():
                 else:
                     for p in plates:
                         lift=Action(name='lift '+b+' to '+p,positive_preconditions=["TruckAt"+loc,"Box"+b+"At"+loc,\
-                            bx+"At"+loc,bx+"plate"+p,"clear"+bx,b+"onfloor"],negative_preconditions=[],add_list=[b+"on"+bx,b+"plate"+p],delete_list=["clear"+bx])
+                            bx+"At"+loc,bx+"plate"+p,"clear"+bx,b+"onfloor"],negative_preconditions=[],add_list=[b+"on"+bx,b+"plate"+p],delete_list=["clear"+bx,b+"onfloor"])
                         actions.append(lift)
                         drop=Action(name='drop '+b+' from '+p,positive_preconditions=["TruckAt"+loc,"Box"+b+"At"+loc,\
-                            b+"plate"+p,"clear"+b],negative_preconditions=[],add_list=[b+"onfloor"],delete_list=[b+"plate"+p])
+                                b+"plate"+p,"clear"+b],negative_preconditions=[],add_list=[b+"onfloor"],delete_list=[b+"plate"+p])
                         actions.append(drop)
-                        
-            load=Action(name='load box'+b+' from '+loc,positive_preconditions=["TruckAt"+loc,"Box"+b+"At"+loc, "clear"+b,b+"onfloor"],\
+                
+            load=Action(name='load box'+b+' from '+loc,positive_preconditions=["TruckAt"+loc,"Box"+b+"At"+loc,b+"onfloor"],\
                 negative_preconditions=[],add_list=[b+"onTruck"],delete_list=[b+"onfloor"])
             unload=Action(name='unload box'+b+' in '+loc,positive_preconditions=["TruckAt"+loc,"Box"+b+"At"+loc,\
                 b+"onTruck"],negative_preconditions=[],add_list=[b+"onfloor"],delete_list=[b+"onTruck"])
